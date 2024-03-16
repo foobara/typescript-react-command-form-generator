797b8a9144b806fb3ba628ea77b8b0200ec19f31
Miles Georgi <azimux@gmail.com>
2024-03-16 12:02:32 -0700

    Load existing manifest from disk if it's available

--- spec/write_typescript_react_command_form_to_disk_spec.rb
+++ spec/write_typescript_react_command_form_to_disk_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe Foobara::Generators::TypescriptReactCommandFormGenerator::WriteTy
   let(:raw_manifest_json) { File.read("spec/fixtures/foobara-manifest.json") }
   let(:raw_manifest) { JSON.parse(raw_manifest_json) }
 
+  after do
+    FileUtils.rm_rf(output_directory)
+  end
+
   it "contains base files" do
     expect(outcome).to be_success
 
@@ -28,6 +32,17 @@ RSpec.describe Foobara::Generators::TypescriptReactCommandFormGenerator::WriteTy
 
     it "is not successful" do
       expect(outcome).to_not be_success
+    end
+
+    context "when a manifest.json is present" do
+      before do
+        described_class.run!(raw_manifest: JSON.parse(raw_manifest_json), output_directory:, command_name:)
+        FileUtils.cp("spec/fixtures/foobara-manifest.json", "#{output_directory}/manifest.json")
+      end
+
+      it "is successful" do
+        expect(outcome).to be_success
+      end
     end
   end
 end
--- src/write_typescript_react_command_form_to_disk.rb
+++ src/write_typescript_react_command_form_to_disk.rb
@@ -4,6 +4,14 @@ module Foobara
   module Generators
     module TypescriptReactCommandFormGenerator
       class WriteTypescriptReactCommandFormToDisk < RemoteGenerator::WriteTypescriptToDisk
+        class NoManifestError < Foobara::RuntimeError
+          class << self
+            def context_type_declaration
+              {}
+            end
+          end
+        end
+
         class << self
           def generator_key
             "typescript-react-command-form"
@@ -20,11 +28,35 @@ module Foobara
         depends_on GenerateTypescriptReactCommandForm
 
         def execute
+          load_manifest_if_needed
           generate_typescript
           write_all_files_to_disk
           run_post_generation_tasks
 
           stats
+        end
+
+        def validate
+          # We don't want to fail if there is no manifest because we might be able to load it from manifest.json
+        end
+
+        def raw_manifest
+          @raw_manifest || inputs[:raw_manifest]
+        end
+
+        def load_manifest_if_needed
+          if !raw_manifest && !manifest_url
+            manifest_path = "#{output_directory}/manifest.json"
+            if File.exist?(manifest_path)
+              @raw_manifest = JSON.parse(File.read(manifest_path))
+            else
+              message = "Because there is no manifest.json to read manifest from, " \
+                        "you must either provide a manifest_url or raw_manifest."
+              error = NoManifestError.new(message:, context: {})
+
+              add_runtime_error(error)
+            end
+          end
         end
 
         def output_directory
